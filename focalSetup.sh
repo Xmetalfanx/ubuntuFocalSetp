8816731fb19740b7888513e4565f101c70f0e1e0
xmetal <xmetalfanx@yahoo.com>
2020-05-11 15:12:04 -0400

    Second Commit
    - honestly too much to list ... just getting the framework setup

--- focalSetup.sh
+++ focalSetup.sh
@@ -1,85 +1,77 @@
 #!/bin/bash
 
+# include Gnome related config file
+. configs/gnomeshell.cfg
 
-function moveShowApplicationsButton()
+
+. configs/gnomeExtConfigs.cfg
+
+## How can i enable the unchecked repos from the command line?
+
+function userPrompt()
 {
-    
-    # Moves it to the top when left aligned ... moves the button to the left when the panel is aligned to the bottom
-    gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
-
+	read -p "Press any key to continue"
 }
 
-
-# Adjusted font size for Ubuntu Dock
-# Source: https://linuxconfig.org/how-to-customize-dock-panel-on-ubuntu-20-04-focal-fossa-linux
-function ubuntuDockFontSize(){
-    gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size $@
-
-}
-
-
-
-# Source: http://ubuntuhandbook.org/index.php/2020/04/dynamic-transparency-ubuntu-20-04/#comment-3670390
-function fixDynamicPanelTransparency()
-{
-    echo -e "Replacing intellifade.js file with fix for new Gnome"
-    sudo cp /assets/intellifade.js ~/.local/share/gnome-shell/extensions/dynamic-panel-transparency@rockon999.github.io/intellifade.js
-
-}
-
-
-#  ~/.local/share/gnome-shell/extensions/dynamic-panel-transparency@rockon999.github.io/intellifade.js
-
-## How can i enable the unchecked repos from the command line? 
-
-# 1 - Update
+# Update
 function updateSystem()
 {
-    sudo apt update 
-    sudo apt upgrade 
-}
-
-
-# 2 - Fix for QT app issue 
-## Source https://www.linuxuprising.com/2018/05/get-qt5-apps-to-use-native-gtk-style-in.html
-
-function fixQTApps(){
-
     clear
+    echo -e "Updating System"
 
-    echo -e "Installing qt5-style-plugins"
-    sudo apt install qt5-style-plugins
+    sudo apt update
+    sudo apt upgrade
 
-
-    echo -e "Exporting QT_QPA_PLATFORMTHEME variable to Home Folder"
-    echo "export QT_QPA_PLATFORMTHEME=gtk2" >> ~/.profile
-
-    echo
-    echo -e "You need to Logout and Login or Reboot for this to take effect"
-    read -p "Press Any Key To Continue"
-
-    # Message to logut and login again here 
-
-
+    echo -e "System Updated"
+    userPrompt
+    
 }
 
 
-# 3 - Gnome Shell related
+# sudo apt install synpatic gdebi
 
-function gnomeShellAdditions()
-{
-    sudo apt install gnome-tweak-tool chrome-gnome-shell gnome-shell-extension-impatence gnome-shell-extension-topiconplus
-    gnome-shell-extension-gsconnect
+function main() {
+
+    while [ $x=1 ]; do
+
+        clear
+
+        echo -e "Xmetal's Ubuntu 20.04 Focal Script\n"
+
+        echo -e "1. \t Update your System ( Snaps and Flatpaks support coming later )"
+        echo -e "2. \t Add some Gnomeshell items from default repos"
+        echo -e "3. \t Fix QT App issue (slow to open)"
+
+        # Tweaks
+        echo -e "4. \t Change default click action on Ubuntu Dock to Maximize/Open and Minimize"
+        echo -e "5. \t Move Show Applications icon to Top (or left, if panel is at the bottom)"
+        echo -e "6. \t Adjust Ubuntu Dock Font Size"
+
+
+        echo -e "r. \t Reset Ubuntu settings to Default"
+        echo -e "q. \t Exit to Prompt \n\n"
+        read -p "Your Choice?:" mainSelection
+
+        case $mainSelection in
+
+        1) updateSystem ;;
+        2) gnomeShellAdditions ;;
+        3) fixQTApps ;;
+        4) minMaxUbuntuDock ;;
+        5) moveShowApplicationsButton ;;
+        6) ubuntuDockFontSize ;;
+
+
+        [rR]) gnomeTweakDefaults ;;
+
+
+        [qQ]) quitScript ;;
+
+        *) invalidSection ;;
+
+        esac
+    done
 
 }
 
-
-
-
-# 4 - 
-
-sudo apt install synpatic gdebi
-
-
-# 5 - Sets left dock to max/min for the action 
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+main
