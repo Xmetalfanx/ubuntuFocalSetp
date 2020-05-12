efdb2d443245b62a30b4dae57005d6884d8cdc8d
xmetal <xmetalfanx@yahoo.com>
2020-05-12 03:50:38 -0400

    Various
    - Moved Gnome Tweaks to it's own Menu
    - Added more Ubuntu/Gnome Tweaks

--- focalSetup.sh
+++ focalSetup.sh
@@ -28,6 +28,49 @@ function updateSystem()
 }
 
 
+function tweaksMenu(){
+while [ $x=1 ]; do
+
+        clear
+
+        echo -e "Xmetal's Ubuntu 20.04 Focal Tweaks Menu\n"
+
+        echo -e "1. \t Change default click action on Ubuntu Dock to Maximize/Open and Minimize"
+        echo -e "2. \t Move Show Applications icon to Top (or left, if panel is at the bottom)"
+        echo -e "3. \t Adjust Ubuntu Dock Icon Size"
+        echo -e "4. \t Move Dock To Bottom"
+        echo -e "5. \t Move Dock to Left"
+
+        echo -e "6. \t Adjust Ubuntu Dock Background"
+
+        echo -e
+        echo -e "r. \t Reset Ubuntu settings to Default"
+        echo -e "q. \t Exit to Prompt \n\n"
+        read -p "Your Choice?:" mainSelection
+
+        case $mainSelection in
+
+        1) minMaxUbuntuDock ;;
+        2) moveShowApplicationsButton ;;
+        3) ubuntuDockFontSize ;;
+        4) moveDock 'BOTTOM' ;;
+        5) moveDock 'LEFT' ;;
+
+        6) ubuntuDockTransparency ;;
+
+        [rR]) gnomeTweakDefaults ;;
+        [qQ]) quitScript ;;
+
+        *) invalidSection ;;
+
+        esac
+    done
+
+}
+
+
+
+
 # sudo apt install synpatic gdebi
 
 function main() {
@@ -41,14 +84,10 @@ function main() {
         echo -e "1. \t Update your System ( Snaps and Flatpaks support coming later )"
         echo -e "2. \t Add some Gnomeshell items from default repos"
         echo -e "3. \t Fix QT App issue (slow to open)"
-
-        # Tweaks
-        echo -e "4. \t Change default click action on Ubuntu Dock to Maximize/Open and Minimize"
-        echo -e "5. \t Move Show Applications icon to Top (or left, if panel is at the bottom)"
-        echo -e "6. \t Adjust Ubuntu Dock Font Size"
+        echo -e
+        echo -e "T. \t Tweak Menu"
 
 
-        echo -e "r. \t Reset Ubuntu settings to Default"
         echo -e "q. \t Exit to Prompt \n\n"
         read -p "Your Choice?:" mainSelection
 
@@ -57,13 +96,9 @@ function main() {
         1) updateSystem ;;
         2) gnomeShellAdditions ;;
         3) fixQTApps ;;
-        4) minMaxUbuntuDock ;;
-        5) moveShowApplicationsButton ;;
-        6) ubuntuDockFontSize ;;
 
 
-        [rR]) gnomeTweakDefaults ;;
-
+        [tT]) tweaksMenu ;;
 
         [qQ]) quitScript ;;
 
